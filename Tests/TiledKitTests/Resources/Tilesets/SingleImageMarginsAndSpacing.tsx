b7a3aebdc22b96ec5a39c3163f8799c6ab967b28
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-09-18 06:15:46 -0500

    Missing files

---
+++ Tests/TiledKitTests/Resources/Tilesets/SingleImageMarginsAndSpacing.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="SingleImageMarginsAndSpacing" tilewidth="12" tileheight="12" spacing="2" margin="2" tilecount="4" columns="2" objectalignment="topleft">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../Images/4 Tiles.png" trans="ff00ff" width="32" height="32"/>
+</tileset>
